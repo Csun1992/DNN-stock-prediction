c8f9498d02b2ad14ce1746cacfe067852346206a
Chong Sun <sonshicyu@gmail.com>
2018-12-24 21:56:07 -0600

    Set up experiments and test run

--- dnn.py
+++ dnn.py
@@ -1,14 +1,15 @@
 import tensorflow as tf
 import sklearn as sk
+from sklearn import model_selection
 import numpy as np
 
-def trainTestSplit(fileName, trainSize, randomState):
-    clusterData = np.loadtxt('../data/clusterData.txt')
-    trainFileName = '../data/' + fileName + 'TrainData.txt'
+def trainTestSplit(fileName, trainSize=0.8, randomState=41):
+    clusterData = np.loadtxt('./data/clusterData.txt')
+    trainFileName = './data/' + fileName + 'TrainData.txt'
     trainData = np.loadtxt(trainFileName)
     label = trainData[:, -1]
     data = np.concatenate((trainData[:,:-1], clusterData), axis=1)
-    XTrain, XTest, yTrain, yTest = sk.model_selection.train_test_split(data, label,
+    XTrain, XTest, yTrain, yTest = model_selection.train_test_split(data, label,
             test_size=1-trainSize, random_state=randomState)
     return (XTrain, XTest, yTrain, yTest) 
 
@@ -20,18 +21,18 @@ trainingPercent = 0.8
 trainingDataSize = int(totalDataSize * trainingPercent)
 testDataSize = totalDataSize - trainingDataSize
 batchSize = 10
-inputNode = 9 # data dimension is 9
-layer1Node, layer2Node = 2 # Two hidden layers, each has two nodes in it
+inputNode = 8 # data dimension is 9
+layer1Node, layer2Node = 2, 2 # Two hidden layers, each has two nodes in it
 outputNode = 1 # binary classification problem 
 learningRateBase = 0.8
 learningRateDecay = 0.99
 regularizationRate = 0.0001
-trainingSteps = 3000
+trainingSteps = tf.constant(3000)
 movingAverageDecay = 0.99
 
 def forwardPropagation(inputTensor, weights1, bias1, weights2, bias2, avgClass):
     if avgClass == None:
-        layer1 = tf.relu(tf.matmul(inputTensor, weights1)+bias1)
+        layer1 = tf.nn.relu(tf.matmul(inputTensor, weights1)+bias1)
         return tf.matmul(layer1, weights2)+bias2
     else:
         layer1 = tf.nn.relu(tf.matmul(inputTensor, avgClass.average(weights1)) +
@@ -43,16 +44,16 @@ def train(stockData):
     y = tf.placeholder(tf.float32, [None, outputNode], name='yInput')
     weights1 = tf.Variable(tf.truncated_normal([inputNode, layer1Node], stddev=0.1))
     bias1 = tf.Variable(tf.constant(0.1, shape=[layer1Node]))
-    weight2 = tf.Variable(tf.truncated_normal([layer1Node, outputNode], stddev=0.1))
+    weights2 = tf.Variable(tf.truncated_normal([layer1Node, outputNode], stddev=0.1))
     bias2 = tf.Variable(tf.constant(0.1, shape=[outputNode]))
     yHat = forwardPropagation(x, weights1, bias1, weights2, bias2, None)
     globalStep = tf.Variable(0, trainable=False)
     variableAverages = tf.train.ExponentialMovingAverage(movingAverageDecay, globalStep)
-    variablesAveragesOp = variableAverages.apply(tf.tranable_variables()) # run moving average on
+    variablesAveragesOp = variableAverages.apply(tf.trainable_variables()) # run moving average on
                                                                           # on all the trainables
-    yAverage = forwardPropagation(x, variableAverages, weights1, bias1, weights2, bias2, None)
+    yAverage = forwardPropagation(x, weights1, bias1, weights2, bias2, variableAverages)
 
-    crossEntropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logtis=y, labels=tf.argmax(yHat, 1))
+    crossEntropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(yHat, 1))
     crossEntropyMean = tf.reduce_mean(crossEntropy)
 
     regularizer = tf.contrib.layers.l2_regularizer(regularizationRate)
@@ -62,28 +63,37 @@ def train(stockData):
     learningRate = tf.train.exponential_decay(learningRateBase, globalStep, trainingDataSize /
             batchSize, learningRateDecay)
 
-    trainStep = tf.train.GradientDescentOptimizer(learningRate).minimize(loss,
-            global_step=globalStep)
-
-trainOp = tf.group(trainStep, variablesAveragesOp)
-
-correctPrediction = tf.equal(tf.argmax(yAverage, 1), tf.argmax(yHat, 1))
-accuracy = tf.reduce_mean(tf.cast(correctPrecision, tf.float32))
-
-appleTrainFeature, appleTestFeature, appleTrainLabel, appleTestLabel = trainTestSplit('apple',
-        trainSize = trainingPercent, randomState = 41)
-
-trainSize = np.size(appleTrainFeature, 0)  
-maxBatchNO = trainSize - batchSize
-
-init = tf.global_variables_initializer()
+    trainStep = tf.train.GradientDescentOptimizer(learningRate).minimize(loss, global_step=globalStep)
     
-with tf.Session() as sess:
-    init.run()
-    testFeed = {x:appleTestFeature, y:appleTestLabel}
-    for i in range(trainingSteps)
-        randomBatch = np.randint(0, maxBatchNO+1)
-        sess.run(trainOp, feed_dict={x:xBatch, y:yBatch})
+    trainOp = tf.group(trainingSteps,variablesAveragesOp)
+    
+    correctPrediction = tf.equal(tf.argmax(yAverage, 1), tf.argmax(yHat, 1))
+    accuracy = tf.reduce_mean(tf.cast(correctPrediction, tf.float32))
+    
+    trainFeature, testFeature, trainLabel, testLabel = trainTestSplit(stockData,
+            trainSize = trainingPercent, randomState = 41)
+    
+    trainSize = np.size(trainFeature, 0)  
+    maxBatchNO = trainSize - batchSize
+    
+    init = tf.global_variables_initializer()
+        
+    with tf.Session() as sess:
+        init.run()
+        for i in range(3000):
+            randomBatch = np.random.randint(0, maxBatchNO+1)
+            testFeed = {x:testFeature[randomBatch:(randomBatch+batchSize), :],
+                y:testLabel[randomBatch:(randomBatch+batchSize)].reshape(-1,1)}
+            sess.run(trainOp, feed_dict=testFeed)
+    
+        testAccuracy  = sess.run(accuracy, feed_dict=testFeed)
+        print testAccuracy
 
-    testAccuracy  = sess.run(accuracy, feed_dict=testFeed)
-    print("The test accuracy using average is %g"%(testAccurarcy))
+
+def main(argv=None):
+    stock = 'apple'
+    train(stock)
+
+
+if __name__ == '__main__':
+    tf.app.run()
